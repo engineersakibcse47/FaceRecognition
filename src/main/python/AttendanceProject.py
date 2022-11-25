4bba016876a4b98ffd9a183c49996245a5c33345
Sakib Hosen Himel <108215990+engineersakibcse47@users.noreply.github.com>
2022-11-26 00:27:04 +0600

    Add files via upload

--- src/main/python/AttendanceProject.py
+++ src/main/python/AttendanceProject.py
@@ -3,53 +3,57 @@ import numpy as np
 import face_recognition
 import os
 from datetime import datetime
-# from PIL import ImageGrab
- 
+
 path = 'ImagesAttendance'
-images = []
+photos = []
 classNames = []
 myList = os.listdir(path)
 print(myList)
 for cl in myList:
     curImg = cv2.imread(f'{path}/{cl}')
-    images.append(curImg)
+    photos.append(curImg)
     classNames.append(os.path.splitext(cl)[0])
 print(classNames)
 
-def findEncodings(images):
-    encodeList = []
-    for img in images:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
-    return encodeList
+
+def findEncodings(photos):
+    cdata = []
+    for i in photos:
+        i = cv2.cvtColor(i, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(i)[0]
+        cdata.append(encode)
+    return cdata
+
+
 def markAttendance(name):
-    with open('Attendance.csv','r+') as f:
-        myDataList = f.readlines()
-        nameList = []
-        for line in myDataList:
+    with open('Attendance.csv', 'r+') as f:
+        mydata = f.readlines()
+        namelist = []
+        for line in mydata:
             entry = line.split(',')
-            nameList.append(entry[0])
-        if name not in nameList:
+            namelist.append(entry[0])
+        if name not in namelist:
             now = datetime.now()
-            dtString = now.strftime('%H:%M:%S')
-            f.writelines(f'\n{name},{dtString}')
-encodeListKnown = findEncodings(images)
+            show = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{show}')
+
+
+encodelistknown = findEncodings(photos)
 print('Encoding Complete')
 
 cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
-    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
     facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodelistknown, encodeFace)
+        faceDis = face_recognition.face_distance(encodelistknown, encodeFace)
         print(faceDis)
         matchIndex = np.argmin(faceDis)
 
