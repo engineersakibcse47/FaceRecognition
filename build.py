495d2b51db4893c3fd85fc76e571c0d8d5486147
Sakib Hosen Himel <108215990+engineersakibcse47@users.noreply.github.com>
2022-11-25 16:00:08 +0600

    Add files via upload

--- build.py
+++ build.py
@@ -2,7 +2,7 @@
 from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
-use_plugin("python.unittest")
+#use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
