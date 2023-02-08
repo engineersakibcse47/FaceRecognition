36bcf0acb59fb185d4416fe572c1301e69b20716
Sakib Hosen Himel <108215990+engineersakibcse47@users.noreply.github.com>
2023-02-08 23:38:11 +0600

    Add files via upload

---
+++ tasks/daily_routine_tckr.dsl
@@ -0,0 +1,55 @@
+~ NB '~' is used for commenting.
+
+~ I created a short quick list of my daily routine habits each weeks written here
+~ We can calculate how much time we utilize in each different activity and of the week.
+~ DSL, Weekly's Routine Cycle :
+
+~ Sunday 
+30 minutes Exercise
+60 minutes Prayer
+30 minutes Read_News
+30 minutes Walk
+60 minutes Homework
+60 minutes Study
+
+~ Monday
+60 minutes Prayer
+30 minutes Walk
+20 minutes Read_News
+30 minutes Cycleing
+2 hours Study
+
+~ Tuesday
+60 minutes Cycleing
+60 minutes Prayer
+2 hours Study
+60 minutes Homework
+
+~ Wednesday
+30 minutes Exercise
+60 minutes Prayer
+30 minutes Walk
+20 minutes Read_News
+60 minutes Study
+40 minutes Homework
+
+~ Thursday
+30 minutes Exercise
+60 minutes Prayer
+25 minutes Read_News
+60 minutes Refreshment
+2 hours Study
+40 minutes Homework
+
+
+~ Overview
+Time spent in Exercise this week.
+Time spent in Prayer this week.
+Time spent in Read_News this week.
+Time spent in Walk this week.
+Time spent in Homework this week.
+Time spent in Cycleing this week.
+Time spent in Study this week.
+Time spent in Refreshment this week.
+
+
---
+++ tasks/dsl_interpreter.py
@@ -0,0 +1,45 @@
+# Import the sys module
+import sys
+
+# Define a dictionary of functions to perform calculation based on time unit
+functions = {'minutes': lambda x, y: x + round(y/60, 2),
+             'hours': lambda x, y: x + y}
+
+# Check if exactly two arguments are given
+if len(sys.argv) != 2:
+    # Exit the program with error code 1 if not
+    sys.exit(1)
+
+# Open the file specified as the second argument
+with open(sys.argv[1]) as file:
+    # Initialize a dictionary to keep track of total time spent on each activity
+    routine = {"Exercise": 0,
+               "Prayer": 0,
+               "Read_News": 0,
+               "Walk": 0,
+               "Homework": 0,
+               "Cycleing": 0,
+               "Study": 0,
+               "Refreshment": 0               
+               }
+
+    # Loop through each line in the file
+    for line in file:
+        # Strip the line of any leading/trailing whitespaces
+        line = line.strip()
+        # Skip empty lines or comments (lines starting with '~')
+        if not line or line[0] == '~':
+            continue
+        # Split the line into separate parts, using spaces as the separator
+        single = line.split()
+        # Check if the first part of the line is 'Time'
+        if single[0] == 'Time':
+            # Print the total time spent on the activity specified in the line
+            print(f"You spent: {routine[single[3]]} hours {single[3]} this week.")
+        else:
+            # Retrieve the current time spent on the activity
+            x = routine[single[2]]
+            # Convert the first part (time value) to integer
+            y = int(single[0])
+            # Update the time spent on the activity by calling the appropriate function
+            routine[single[2]] = functions[single[1]](x, y)
